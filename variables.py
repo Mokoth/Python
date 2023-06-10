c3083ad933dac5f05406dd6e25a85484bf82e71d
Mokoth <onyach.okoth1@outlook.com>
2023-06-10 20:15:25 +0300

    Add file

---
+++ variables.py
@@ -0,0 +1,7 @@
+# method of storing data for reuse
+just_a_variable = 'I am a variable'
+print(just_a_variable)
+
+# variable update
+just_a_variable = 'I am an updated variable'
+print(just_a_variable)
