7bad0a53e04e1702059501aa97a1623a5fa6890f
Mark Onyach <onyach.okoth1@outlook.com>
2024-03-03 07:37:25 +0000

    update file

---
+++ app/main.py
@@ -0,0 +1,26 @@
+# print()
+# EOL 
+
+# VALUE AND TYPES
+    # strings - > each letter (binary value)
+    # integer
+    # float
+    # boolean True/False
+
+    # type() -> check value type
+
+# VARIABLES
+    # assignment (=)
+spam = 10
+message = 'get back home'
+PI = 3.142144
+    # variable assignment
+spam = 'hello'
+
+# OPERATORS
+    # + / - % // **
+print(50 / 2) # dividing always retuns float output
+
+# INPUT
+name = input('Enter name: ')
+# \n -> NEWLINE
