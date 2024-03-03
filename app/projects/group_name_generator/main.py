66d412ce588222c26152ab19bd3030577b706346
Mark Onyach <onyach.okoth1@outlook.com>
2024-03-03 09:48:01 +0000

    update file

---
+++ app/projects/group_name_generator/main.py
@@ -0,0 +1,13 @@
+# Group Name Project
+
+# pseudocode
+
+# Create a greeting for your program.
+# Ask the user for their favorite color.
+# Ask the user for their favotire animal.
+# Combine the name of their favorite color and animal and show them their group name.
+
+print('Welcome to the Group Name Generator.\n')
+color = input("What's your favorite color?\n> ")
+animal = input("What's your favorite animal?\n> " )
+print(f"Your group name could be {color.capitalize()} {animal.capitalize()}")
