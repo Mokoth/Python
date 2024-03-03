65eeb6135e331475679f810d3d02a57eb1a3018b
Mark Onyach <onyach.okoth1@outlook.com>
2024-03-03 08:31:07 +0000

    update file

--- app/main.py
+++ app/main.py
@@ -21,6 +21,26 @@ spam = 'hello'
     # + / - % // **
 print(50 / 2) # dividing always retuns float output
 
+
+
 # INPUT
 name = input('Enter name: ')
 # \n -> NEWLINE
+
+
+# user_name = input('What is your name?\n')
+# print(user_name)
+
+
+years = input('Enter number of years ')
+no_weeks = int(years) * 52
+
+print('There are ' + str(no_weeks) +  ' weeks in ' + str(years) + ' years')
+
+# f-string
+# print(f"There are {no_weeks} weeks in {years} years")
+
+# Round
+print(10 / 3) # 3.3333333333333335
+print(int(10 / 3)) # 3
+print(round(10 / 3, 2)) # 3.33
