c3431c1c482bb7120b522fa42f67d24918de0c25
Mark Onyach <onyach.okoth1@outlook.com>
2024-03-21 12:46:56 +0000

    update file

--- app/projects/gross_pay/main.py
+++ app/projects/gross_pay/main.py
@@ -3,10 +3,10 @@
 # Write a program to prompt the user for hours and rate per hour to compute gross pay. 
 # You need to take into account that the result has exactly two digits after the decimal place.
 
-hours = float(input('Enter hours: '))
-rate = float(input('Enter company rate: '))
+# hours = float(input('Enter hours: '))
+# rate = float(input('Enter company rate: '))
 
-normal_pay = hours * rate
+# normal_pay = hours * rate
 # print(round(gross_pay, 2))
 
 
@@ -27,10 +27,39 @@ normal_pay = hours * rate
 #     print(gross_pay)
 
 
+# if hours > 40:
+#     overtime = hours - 40
+#     overtime_pay = (overtime * 1.5) * rate
+#     gross_pay = (40 * rate) + overtime_pay
+#     print(gross_pay)
+# else:
+#     print(normal_pay)
+
+
+# with try except
+# Rewrite Gross Pay program using try and except so that your program handles 
+# non-numeric input gracefully by printing a message and exiting the program.
+
+
+    # how to do for both - hours and rate
+try:
+    hours = float(input("Enter hours: "))
+except ValueError:
+    print(f'Enter a numeric value.')
+    quit()
+try:
+    rate = float(input("Enter rates: "))
+except ValueError:
+    print("Enter numeric values")
+    quit()
+
 if hours > 40:
     overtime = hours - 40
-    overtime_pay = (overtime * 1.5) * rate
-    gross_pay = (40 * rate) + overtime_pay
-    print(gross_pay)
+    overtime_pay = overtime * 1.5 * rate
+    total_pay = (40 * rate) + overtime_pay
+    print(total_pay)
 else:
-    print(normal_pay)
+    total_pay = hours * rate
+    print(total_pay)
+# else:
+#     print(f'{rate} {hours}')
