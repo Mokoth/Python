45e2ced0cbe43549ac4a8c03e27403e01ee3bd9b
Mark Onyach <onyach.okoth1@outlook.com>
2024-03-22 15:49:30 +0000

    create files

---
+++ app/modules/main.py
@@ -0,0 +1,31 @@
+# import own module
+
+# import marks_module
+
+# result = marks_module.PI
+
+# print(result)
+
+
+import math
+
+# pi from math
+area = math.pi * 4 * 4
+
+# print(area)
+
+# factorial from math
+# num = int(input("Enter a number: "))
+# fact = math.factorial(num)
+
+# random
+import random
+
+# randint()
+
+# random_int = random.randint(1, 10)
+# print(random_int)
+
+# random() - generate float numbers 0.0 - 1.0(excluded) => 0.99
+result = random.random()
+print(result)
---
+++ app/modules/marks_module.py
@@ -0,0 +1,4 @@
+# Working with modules
+# Create own module
+
+PI = 3.14
