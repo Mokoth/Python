ec97694203a4d2f6aea7f562892a5c5b72ea64f1
Mark Onyach <onyach.okoth1@outlook.com>
2024-03-29 14:09:08 +0000

    update file

--- DA/main.py
+++ DA/main.py
@@ -36,3 +36,94 @@ sum_of_ratings = sum(rating_sum)
 avg_rating = sum_of_ratings / len(apps_data)
 # Print the results of avg_rating.
 print(avg_rating)
+
+# # Task
+# Convert the AppleStore.csv file into a list of lists and assign that list to a variable named apps_data.
+
+# Create an empty list named ratings.
+# Iterate over apps_data[1:] (which excludes the header row), and for each iteration (row), 
+# we do the following:
+# Extract the rating and convert it to a float using float(row[7]). 
+# The rating has the index number 7 and comes as a string, so we need to convert it to a float.
+# We assign the rating to a variable named rating.
+# We append rating to the ratings list we created outside the loop using ratings.append(rating) function.
+
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+ratings = []
+for row in apps_data[1:]:
+    rating = float(row[7])
+    ratings.append(rating)
+print(ratings[:5])
+
+# Task
+
+# Inside the for loop, complete the following:
+# Get the name of the app and store it in a variable called name. 
+# The name is the second item in each row.
+# Append the value stored in name to the apps_names list using the list_name.append() function
+#  (note the apps_names list is already defined in the code editor) and be careful with indentation.
+# Print the first 5 elements in apps_names list to display the names of the apps.
+
+opened_file = open('AppleStore.csv')
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+apps_names = []
+for row in apps_data[1:]:
+    name = row[1]
+    apps_names.append(name)
+print(apps_names[:5])
+
+
+# In the diagram below, we created a list of lists named app_and_price, and we want to extract the names of the free apps in a separate list. To do that, complete the following:
+
+# Create an empty list named free_apps.
+# Iterate over app_and_price. For each iteration, we do the following:
+# Extract the name of the app and assign it to a variable named name.
+# Extract the price of the app and assign it to a variable named price.
+# Append the name of the app to free_apps (the empty list that we initialized outside the loop) if the price of the app equals 0.
+
+app_and_price = [['Facebook', 0], ['Instagram', 0], ['Tiktok', 2.99], ['Temple Run', 4.55], ['Zombies', 0]]
+
+free_apps = []
+for row in app_and_price:
+    name = row[0]
+    price = row[1]
+
+    if price == 0:
+        free_apps.append(name)
+
+print(free_apps)
+
+# Task
+
+# Using the same techniques in the diagram above, compute the average rating of non-gaming apps.
+
+# Initialize an empty list named non_games_ratings.
+# Loop through the apps_data list of lists (make sure you don't include the header row). For each iteration of the loop, do the following:
+# Assign the rating of the app as a float to a variable named rating (the index number of the rating column is 7).
+# Assign the genre of the app to a variable named genre (index number 11).
+# If the genre is not equal to 'Games', append the rating to the non_games_ratings list.
+# Compute the average rating of non-gaming apps and assign the result to a variable named avg_rating_non_games.
+
+# Optional exercise: compare the average rating of gaming apps (3.69) with the average rating of non-gaming apps. Why do you think we see this difference?
+
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+non_games_ratings = []
+for row in apps_data[1:]:
+    rating = float(row[7])
+    # print(type(rating))
+    genre = row[11]
+    if genre != 'Games':
+        non_games_ratings.append(rating)
+# print(avg)
+avg_rating_non_games = sum(non_games_ratings) / len(non_games_ratings)
+print(avg_rating_non_games)
